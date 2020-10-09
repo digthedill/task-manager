a476481441de1cce2fe55b23d3b6358b85d24d5d
digthedill <popssly@gmail.com>
2020-10-09 10:11:03 -0500

    updated files

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,15 +5,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
-const Tasks = mongoose.model("Tasks", {
-  description: {
-    trim: true,
-    required: true,
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
